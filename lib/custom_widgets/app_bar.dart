bd9fa5082777bdf6254c1b7125ea8fcd57f70144
Imgkl <saigklkrishnan@gmail.com>
2020-12-25 18:16:48 +0530

    abstracted the toggle widget into new widget

--- lib/custom_widgets/app_bar.dart
+++ lib/custom_widgets/app_bar.dart
@@ -1,10 +1,7 @@
 import 'package:flikipedia/custom_widgets/dynamic_text.dart';
-import 'package:flikipedia/utils/theme_controller.dart';
+import 'package:flikipedia/custom_widgets/theme_toggle.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:line_icons/line_icons.dart';
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class Appbar extends StatefulWidget {
   @override
@@ -33,53 +30,13 @@ class _AppbarState extends State<Appbar> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.only(top: 58.0, right: 10, left: 10),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          GestureDetector(
-            onTap: () {
-              if (Get.theme.brightness == Brightness.light) {
-                ThemeController.to.setThemeMode(ThemeMode.dark);
-              } else {
-                ThemeController.to.setThemeMode(ThemeMode.light);
-              }
-              changeThemeMode();
-            },
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  width: width * 0.1,
-                  height: width * 0.1,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: Get.theme.brightness == Brightness.light
-                          ? [
-                              const Color(0xFF8983F7),
-                              const Color(0xFFA3DAFB),
-                            ]
-                          : [
-                              const Color(0xDDFF0080),
-                              const Color(0xDDFF8C00),
-                            ],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                  ),
-                ),
-                Positioned.fill(
-                  child: Icon(Get.theme.brightness == Brightness.light
-                      ? LineIcons.sun_o
-                      : FontAwesomeIcons.moon),
-                ),
-              ],
-            ),
-          ),
+          ThemeToggle(changeThemeMode()),
           Align(
             alignment: Alignment.centerRight,
             child: Text(
---
+++ lib/custom_widgets/theme_toggle.dart
@@ -0,0 +1,53 @@
+import 'package:flikipedia/utils/theme_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:line_icons/line_icons.dart';
+
+class ThemeToggle extends StatelessWidget {
+  final Function toggleFunction;
+
+  const ThemeToggle(this.toggleFunction);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (Get.theme.brightness == Brightness.light) {
+          ThemeController.to.setThemeMode(ThemeMode.dark);
+        } else {
+          ThemeController.to.setThemeMode(ThemeMode.light);
+        }
+        toggleFunction();
+      },
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 35,
+            height: 35,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: Get.theme.brightness == Brightness.light
+                    ? [
+                        const Color(0xFF8983F7),
+                        const Color(0xFFA3DAFB),
+                      ]
+                    : [
+                        const Color(0xDDFF0080),
+                        const Color(0xDDFF8C00),
+                      ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Icon(Get.theme.brightness == Brightness.light
+                ? LineIcons.sun_o
+                : FontAwesomeIcons.moon),
+          ),
+        ],
+      ),
+    );
+  }
+}
